b151f52de26e898ae81dd7d3655900234d29fac0
EC2 Default User <ec2-user@ip-172-31-43-183.us-east-2.compute.internal>
2019-07-24 15:32:42 +0000

    first

---
+++ app/models/model.py
@@ -0,0 +1,5 @@
+def reverseit(word):
+    print(word[::-1])
+    return word[::-1]
+
+print(reverseit("hello"))
---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods = ['GET', 'POST'])
+def results():
+    userdata = dict(request.form)
+    print(userdata) #What is userdata??
+    word = userdata['word']
+    print(word) #What is word??
+    reversed = model.reverseit(word) ##going to model file
+    ##calling reverseit
+    ##passing in word that we stored
+    ##storing the return value from that function as reversed
+    print(reversed)
+    return render_template('results.html', reversed = reversed)
